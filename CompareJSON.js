32237e0f9c8130aa20ec7e2f5b6e1f6b73538a54
Sumitha2502 <sumitha0996@gmail.com>
2024-02-20 14:13:59 +0530

    JSD3DisplayRestCountries

--- CompareJSON.js
+++ CompareJSON.js
@@ -1,23 +1,25 @@
 var obj1 = { name:"Person 1",age:5 };
-var obj2 = { age:5,name:"Person 1"};
+var obj2 = { age:10,name:"Person 1"};
+
+
+let flag=true;
+
+for(let e in obj1)
+{
+    if(obj1[e] !== obj2[e])
+    {
+        flag= false
+        break
+    }
+}
+flag?console.log("Equal"):console.log("Not Equal")
+
 
 //JSON stringify prints- order
-if(JSON.stringify(obj1) === JSON.stringify(obj2)){
-    console.log("Equal");
-}
-else{
-    console.log("Not Equal")
-}
-
-
-// let flag=true;
-
-// for(let e in obj1)
-// {
-//     if(obj1[e] !== obj2[e])
-//     {
-//         flag= false
-//         break
-//     }
+// if(JSON.stringify(obj1) === JSON.stringify(obj2)){
+//     console.log("Equal");
 // }
-// flag?console.log("Equal"):console.log("Not Equal")
+// else{
+//     console.log("Not Equal")
+// }
+
