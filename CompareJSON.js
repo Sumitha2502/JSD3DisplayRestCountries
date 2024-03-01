7b9cf018ec8c126cd85a1732850ead56dc59b96f
Sumitha2502 <130633037+Sumitha2502@users.noreply.github.com>
2024-03-01 13:02:18 +0530

    Update CompareJSON.js

--- CompareJSON.js
+++ CompareJSON.js
@@ -1,24 +1,23 @@
-var obj1 = { name:"Person 1",age:5 };
-var obj2 = { age:10,name:"Person 1"};
-
-let flag=true;
-
-for(let e in obj1)
-{
-    if(obj1[e] !== obj2[e])
-    {
-        flag= false
-        break
-    }
+var obj1 = { name:"Person 1",age:5};
+var obj2 = { age:5,name:"Person 1"};
+if(JSON.stringify(obj1) === JSON.stringify(obj2)){
+    console.log("Equal");
 }
-flag?console.log("Equal"):console.log("Not Equal")
+else{
+    console.log("Not Equal")
+}
+
 
 
 //JSON stringify prints- order
-// if(JSON.stringify(obj1) === JSON.stringify(obj2)){
-//     console.log("Equal");
-// }
-// else{
-//     console.log("Not Equal")
-// }
+// let flag=true;
 
+// for(let e in obj1)
+// {
+//     if(obj1[e] !== obj2[e])
+//     {
+//         flag= false
+//         break
+//     }
+// }
+// flag?console.log("Equal"):console.log("Not Equal")
